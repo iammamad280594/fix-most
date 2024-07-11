a5046fe3c058b034051b9e68cc0c3a83b028962a
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-07-11 21:03:43 +0200

    Update route.ts

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -5,7 +5,7 @@ export async function POST(req: NextRequest) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
+    throw new Error('ywhsec_e3mLwmTpvWkDDS+CQ4IA1q7JsS5s4BV1');
   }
 
   const { headers } = req;
